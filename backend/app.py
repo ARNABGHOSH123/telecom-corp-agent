e3eea0865056846369c99be5f9a1c3930d6c3045
Arnab Ghosh <arnab.ghosh@fractal.ai>
2025-03-10 17:42:07 +0530

    uvicorn changes

--- backend/app.py
+++ backend/app.py
@@ -3,10 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from models.request_models import ErrorQuery
 from services.multi_agent_service import handle_query
 from dotenv import load_dotenv
+import uvicorn
+import os
 
 app = FastAPI()
 
-origins = ["http://localhost:5173"]
+origins = ["https://telecom-corp-agent-arnab.netlify.app"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -24,3 +26,7 @@ def process_query(request: ErrorQuery):
     """API endpoint to handle user queries."""
     result = handle_query(request.query)
     return {"response": result}
+
+if __name__== '__main__':
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app,host="0.0.0.0", port=port)
